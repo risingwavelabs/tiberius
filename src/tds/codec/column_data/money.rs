22ca2a787bbcbb44f1668f164319f4d31989e8ce
congyi <15605187270@163.com>
2025-05-12 13:07:40 +0800

    change money to i64

--- src/tds/codec/column_data/money.rs
+++ src/tds/codec/column_data/money.rs
@@ -6,17 +6,17 @@ where
 {
     let res = match len {
         0 => ColumnData::I64(None),
-        4 => ColumnData::I64(Some(src.read_i32_le().await? as i64 / 1e4 as i64)),
-        8 => ColumnData::I64(Some({
+        4 => ColumnData::I64(Some(src.read_i32_le().await? as i64)),
+        8 => {
             let high = src.read_i32_le().await? as i64;
-            let low = src.read_u32_le().await? as i64;
+            let low = src.read_u32_le().await?;
 
-            (high << 32) + low / 1e4 as i64
-        })),
+            ColumnData::I64(Some((high << 32) | low as i64))
+        }
         _ => {
             return Err(Error::Protocol(
                 format!("money: length of {} is invalid", len).into(),
-            ));
+            ))
         }
     };
 
